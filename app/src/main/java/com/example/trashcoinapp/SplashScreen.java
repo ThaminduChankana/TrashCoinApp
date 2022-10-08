225e6fe6e336f7940a72c5178e9fca91bf7ec035
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-08 22:07:56 +0530

    started working on the project

---
+++ app/src/main/java/com/example/trashcoinapp/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.example.trashcoinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.lang.reflect.WildcardType;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation logoAnim, innoAnim,teamAnim, quoteAnim;
+    ImageView logo,innovation,team,quote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        logoAnim = AnimationUtils.loadAnimation(this,R.anim.logo_animation);
+        quoteAnim = AnimationUtils.loadAnimation(this,R.anim.quote_animation);
+        innoAnim = AnimationUtils.loadAnimation(this,R.anim.innovation_animation);
+        teamAnim = AnimationUtils.loadAnimation(this,R.anim.team_animation);
+
+        logo = findViewById(R.id.img_logo_name);
+        quote = findViewById(R.id.img_quote);
+        innovation = findViewById(R.id.img_innovation);
+        team = findViewById(R.id.img_team_name);
+
+        logo.setAnimation(logoAnim);
+        quote.setAnimation(quoteAnim);
+        innovation.setAnimation(innoAnim);
+        team.setAnimation(teamAnim);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, IntroActivity.class));
+                finish();
+            }
+        },4000);
+
+    }
+}
