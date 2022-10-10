016eb0f5b763155037bb5d16bec1367cedb7cea5
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-11 01:44:20 +0530

    designed register ui

--- app/src/main/java/com/example/trashcoinapp/GetStarted.java
+++ app/src/main/java/com/example/trashcoinapp/GetStarted.java
@@ -2,10 +2,16 @@ package com.example.trashcoinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
+
+    Button btn_get_started_register;
+    Button btn_get_started_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,17 @@ public class GetStarted extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_get_started);
+
+        btn_get_started_register= findViewById(R.id.btn_get_started_register);
+        btn_get_started_login= findViewById(R.id.btn_get_started_login);
+
+        btn_get_started_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/trashcoinapp/Register.java
@@ -0,0 +1,18 @@
+package com.example.trashcoinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_register);
+
+    }
+}
