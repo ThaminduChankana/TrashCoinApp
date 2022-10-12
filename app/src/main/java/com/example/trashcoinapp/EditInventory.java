4778b4eeabaa12b4f3d34f8c856cb5e9aead7cb7
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-13 01:57:29 +0530

    created add inventory and edit inventory user interfaces

---
+++ app/src/main/java/com/example/trashcoinapp/EditInventory.java
@@ -0,0 +1,17 @@
+package com.example.trashcoinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class EditInventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_edit_inventory);
+    }
+}
