c22aaf10d7c9186b8ebf869c4c54f3d823a85f38
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-12 14:23:11 +0530

    created login for users

--- app/src/main/java/com/example/trashcoinapp/LoginSelector.java
+++ app/src/main/java/com/example/trashcoinapp/LoginSelector.java
@@ -2,7 +2,9 @@ package com.example.trashcoinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class LoginSelector extends AppCompatActivity {
@@ -13,7 +15,18 @@ public class LoginSelector extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_selector);
+    }
 
-
+    public void openWasteDisposerLogin(View view){
+        Intent intent = new Intent(this, LoginWasteDisposer.class);
+        startActivity(intent);
+    }
+    public void openWasteCollectorLogin(View view){
+        Intent intent = new Intent(this, LoginWasteCollector.class);
+        startActivity(intent);
+    }
+    public void openWasteRecyclerLogin(View view){
+        Intent intent = new Intent(this, LoginWasteRecycler.class);
+        startActivity(intent);
     }
 }
