94fd465014ec7e8863a3a9dd38190861e8b042a4
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-14 23:08:07 +0530

    designed chat layouts

---
+++ app/src/main/java/com/example/trashcoinapp/activities/MessagingActivity.java
@@ -0,0 +1,19 @@
+package com.example.trashcoinapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.trashcoinapp.R;
+
+public class MessagingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_messaging);
+    }
+}
