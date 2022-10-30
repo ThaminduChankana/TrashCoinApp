9dad59ed8147b3a9237f4c63dede35bfd646cb96
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-10-30 20:47:57 +0530

    Create a activity for display the products for waste disposer

---
+++ app/src/main/java/com/example/trashcoinapp/activities/ProductViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.trashcoinapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trashcoinapp.R;
+
+public class ProductViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_view);
+    }
+}
