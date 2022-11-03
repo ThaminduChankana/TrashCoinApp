1dbee08bc236c297928b22a0d40286564d8a4083
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-11-03 18:28:01 +0530

    Implement view for one cart item

--- app/src/main/java/com/example/trashcoinapp/activities/cart/CartActivity.java
+++ app/src/main/java/com/example/trashcoinapp/activities/cart/CartActivity.java
@@ -1,10 +1,16 @@
 package com.example.trashcoinapp.activities.cart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.example.trashcoinapp.R;
+import com.example.trashcoinapp.activities.dashboards.WasteDisposerDashboard;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -12,5 +18,39 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_disposer);
+        bottomNavigationView.setSelectedItemId(R.id.img_shopping_cart);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.img_disposer_home:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_view_collectors:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_shopping_cart:
+                        return true;
+                    case R.id.img_waste_in_hand:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_collector_chat:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
