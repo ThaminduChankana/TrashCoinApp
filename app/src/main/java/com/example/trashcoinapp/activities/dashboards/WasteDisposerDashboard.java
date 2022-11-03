68cc13c68a36b20579da1a0ab307afc8c32b40ab
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-11-03 15:46:08 +0530

    Implement waste disposer dashboard

--- app/src/main/java/com/example/trashcoinapp/activities/dashboards/WasteDisposerDashboard.java
+++ app/src/main/java/com/example/trashcoinapp/activities/dashboards/WasteDisposerDashboard.java
@@ -1,13 +1,35 @@
 package com.example.trashcoinapp.activities.dashboards;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.trashcoinapp.R;
+import com.example.trashcoinapp.activities.LoginSelector;
+import com.example.trashcoinapp.activities.cart.ProductViewActivity;
+import com.example.trashcoinapp.activities.chat.Chat;
+import com.example.trashcoinapp.utilities.Constants;
+import com.example.trashcoinapp.utilities.PreferenceManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class WasteDisposerDashboard extends AppCompatActivity {
+
+    private PreferenceManager preferenceManager;
+    private CardView cv_wd_db_shop;
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +37,78 @@ public class WasteDisposerDashboard extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_waste_disposer_dashboard);
+
+        logout = findViewById(R.id.btn_logout);
+        cv_wd_db_shop = findViewById(R.id.cv_wd_db_shop);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+        cv_wd_db_shop .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProductViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_disposer);
+        bottomNavigationView.setSelectedItemId(R.id.img_disposer_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.img_disposer_home:
+                        return true;
+                    case R.id.img_view_collectors:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_shopping_cart:
+                        startActivity(new Intent(getApplicationContext(), ProductViewActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_waste_in_hand:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_collector_chat:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signOut(){
+        showToast("Signing Out ...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), LoginSelector.class));
+                    finish();
+                })
+                .addOnFailureListener((e -> showToast("Unable To Sign Out !")));
     }
 }
