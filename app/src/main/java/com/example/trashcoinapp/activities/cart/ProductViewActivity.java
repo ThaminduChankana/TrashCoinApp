d386c2b5637a58896c43f52e3352aa6d09689bf4
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-11-03 16:26:15 +0530

    Add nav bar to product view

--- app/src/main/java/com/example/trashcoinapp/activities/cart/ProductViewActivity.java
+++ app/src/main/java/com/example/trashcoinapp/activities/cart/ProductViewActivity.java
@@ -1,16 +1,71 @@
 package com.example.trashcoinapp.activities.cart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.example.trashcoinapp.R;
+import com.example.trashcoinapp.activities.dashboards.WasteDisposerDashboard;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProductViewActivity extends AppCompatActivity {
+
+    ImageView img_shopping_cart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_product_view);
+
+        img_shopping_cart = findViewById(R.id.img_shopping_cart);
+
+        img_shopping_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_disposer);
+        bottomNavigationView.setSelectedItemId(R.id.img_shopping_cart);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.img_disposer_home:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_view_collectors:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_shopping_cart:
+                        return true;
+                    case R.id.img_waste_in_hand:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.img_collector_chat:
+                        startActivity(new Intent(getApplicationContext(), WasteDisposerDashboard.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
