5bdcfa540a3587a859d6ad70d9ba7dd67994ba24
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-11-05 17:32:25 +0530

    created edit inventory activity

--- app/src/main/java/com/example/trashcoinapp/activities/inventory/EditInventory.java
+++ app/src/main/java/com/example/trashcoinapp/activities/inventory/EditInventory.java
@@ -1,13 +1,15 @@
-package com.example.trashcoinapp.activities;
+package com.example.trashcoinapp.activities.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 
 import com.example.trashcoinapp.R;
 
@@ -16,6 +18,7 @@ import java.util.Calendar;
 public class EditInventory extends AppCompatActivity {
 
     private Button edt_inv_pick_date;
+    private ImageView img_edit_inventory_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,15 @@ public class EditInventory extends AppCompatActivity {
         setContentView(R.layout.activity_edit_inventory);
 
         edt_inv_pick_date = findViewById(R.id.edt_inv_pick_date);
+        img_edit_inventory_back = findViewById(R.id.img_edit_inventory_back);
+
+        img_edit_inventory_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InventoryActivity.class);
+                startActivity(intent);
+            }
+        });
 
         edt_inv_pick_date.setOnClickListener(new View.OnClickListener() {
             @Override
