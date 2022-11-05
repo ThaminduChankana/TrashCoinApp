d3dc8a4fb6c83bc9063382d2743205b57655583b
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-11-05 17:32:10 +0530

    added add inventory activity

--- app/src/main/java/com/example/trashcoinapp/activities/inventory/AddInventory.java
+++ app/src/main/java/com/example/trashcoinapp/activities/inventory/AddInventory.java
@@ -1,21 +1,32 @@
-package com.example.trashcoinapp.activities;
+package com.example.trashcoinapp.activities.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.trashcoinapp.R;
+import com.example.trashcoinapp.activities.dashboards.WasteCollectorDashboard;
+import com.example.trashcoinapp.database.DBHelper;
 
 import java.util.Calendar;
 
 public class AddInventory extends AppCompatActivity {
 
     private Button add_inv_pick_date;
+    private EditText et_add_inventory_name;
+    private EditText et_add_inventory_description;
+    private Button btn_add_inventory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,19 @@ public class AddInventory extends AppCompatActivity {
         setContentView(R.layout.activity_add_inventory);
 
         add_inv_pick_date = findViewById(R.id.add_inv_pick_date);
+        et_add_inventory_name = findViewById(R.id.et_add_inventory_name);
+        et_add_inventory_description = findViewById(R.id.et_add_inventory_description);
+        btn_add_inventory = findViewById(R.id.btn_add_inventory);
+
+        ImageView img_add_inventory_back = findViewById(R.id.img_add_inventory_back);
+
+        img_add_inventory_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InventoryActivity.class);
+                startActivity(intent);
+            }
+        });
 
         add_inv_pick_date.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +76,34 @@ public class AddInventory extends AppCompatActivity {
             }
         });
 
+        btn_add_inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inventoryName = et_add_inventory_name.getText().toString();
+                String inventoryDescription = et_add_inventory_description.getText().toString();
+                String inventoryAddDate = add_inv_pick_date.getText().toString();
+
+                if(inventoryName.isEmpty()){
+                    et_add_inventory_name.setError("Enter Item Name !");
+                }
+                else if (inventoryDescription.isEmpty()){
+                    et_add_inventory_description.setError("Enter Item Description !");
+                }
+                else if (inventoryAddDate.equals("Pickup Date")){
+                    add_inv_pick_date.setError("Enter Pickup Date !");
+                }
+                else{
+                    DBHelper helper = new DBHelper(AddInventory.this);
+                    helper.addInventory(inventoryName,
+                            inventoryDescription,
+                            inventoryAddDate);
+                }
+
+
+
+            }
+        });
 
     }
 }
