73e75785a11df32a5ea2a2269e1ffbadc5887f32
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-11-09 01:36:26 +0530

    added single login functionality

--- app/src/main/java/com/example/trashcoinapp/activities/SplashScreen.java
+++ app/src/main/java/com/example/trashcoinapp/activities/SplashScreen.java
@@ -11,11 +11,18 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.example.trashcoinapp.R;
+import com.example.trashcoinapp.activities.dashboards.WasteCollectorDashboard;
+import com.example.trashcoinapp.activities.dashboards.WasteDisposerDashboard;
+import com.example.trashcoinapp.activities.dashboards.WasteRecyclerDashboard;
+import com.example.trashcoinapp.utilities.Constants;
+import com.example.trashcoinapp.utilities.PreferenceManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     Animation logoAnim, innoAnim,teamAnim, quoteAnim;
     ImageView logo,innovation,team,quote;
+    private PreferenceManager preference;
+    String role;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,12 @@ public class SplashScreen extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        preference = new PreferenceManager(getApplicationContext());
+        if(preference==null){
+            role="";
+        }
+        role = preference.getString(Constants.KEY_USER_TYPE);
 
         logoAnim = AnimationUtils.loadAnimation(this,R.anim.logo_animation);
         quoteAnim = AnimationUtils.loadAnimation(this,R.anim.quote_animation);
@@ -43,8 +56,29 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, IntroActivity.class));
-                finish();
+
+
+                try{
+                    if(role.equals("Waste Collector")){
+                        startActivity(new Intent(SplashScreen.this, WasteCollectorDashboard.class));
+                        finish();
+                    }else if(role.equals("Waste Disposer")){
+                        startActivity(new Intent(SplashScreen.this, WasteDisposerDashboard.class));
+                        finish();
+                    }else if(role.equals("Waste Recycler")){
+                        startActivity(new Intent(SplashScreen.this, WasteRecyclerDashboard.class));
+                        finish();
+                    }
+
+                }catch(Exception e){
+
+                    startActivity(new Intent(SplashScreen.this, IntroActivity.class));
+                    finish();
+                }
+
+
+
+
             }
         },4000);
 
