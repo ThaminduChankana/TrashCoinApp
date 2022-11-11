b0f71fc1c33ff6d4ec70c5809476f6994a5cf3dd
Charith Lakshan <it20606374@my.sliit.lk>
2022-11-11 23:55:01 +0530

    Passed id to waste recycler view

--- app/src/main/java/com/example/trashcoinapp/activities/recyclerProduct/RecyclerProductView.java
+++ app/src/main/java/com/example/trashcoinapp/activities/recyclerProduct/RecyclerProductView.java
@@ -84,7 +84,7 @@ public class RecyclerProductView extends AppCompatActivity {
 
         recyclerProductViewAdapter = new RecyclerProductViewAdapter(productArrayList, this);
         productWrRV.setAdapter(recyclerProductViewAdapter);
-        db.collection("Products").get()
+        db.collection("Products").whereEqualTo("userID", userId).get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
